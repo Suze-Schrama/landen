b04e6a0fc96576b5366e421ef354669cffa27ce3
Suze Schrama <suze.schrama@gmail.com>
2024-09-16 11:49:10 +0200

    first commit

---
+++ src/main/java/be/vdab/landen/GebruikerController.java
@@ -0,0 +1,21 @@
+package be.vdab.landen;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+@RequestMapping("/")
+public class GebruikerController {
+    GebruikerController() throws IOException {
+var homeDirectory = Path.of(System.getProperty("user.home"));
+var bestand = homeDirectory.resolve("organisatie.txt");
+if (!Files.exists(bestand)){
+    Files.writeString(bestand, "VDAB");
+    System.out.println(bestand + " gemaakt");
+}
+    }
+}
