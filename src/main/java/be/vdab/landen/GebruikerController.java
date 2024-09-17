785a118a755f1e925c499417aea2dc809c77080d
Suze Schrama <suze.schrama@gmail.com>
2024-09-17 09:48:25 +0200

    first commit

--- src/main/java/be/vdab/landen/GebruikerController.java
+++ src/main/java/be/vdab/landen/GebruikerController.java
@@ -1,7 +1,6 @@
 package be.vdab.landen;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,13 +8,19 @@ import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GebruikerController {
     GebruikerController() throws IOException {
-var homeDirectory = Path.of(System.getProperty("user.home"));
-var bestand = homeDirectory.resolve("organisatie.txt");
-if (!Files.exists(bestand)){
-    Files.writeString(bestand, "VDAB");
-    System.out.println(bestand + " gemaakt");
-}
+        var homeDirectory = Path.of(System.getProperty("user.home"));
+        var bestand = homeDirectory.resolve("organisatie.txt");
+        if (!Files.exists(bestand)) {
+            Files.writeString(bestand, "VDAB");
+            System.out.println(bestand + " gemaakt");
+        }
+    }
+
+    @GetMapping("/user")
+    public String getUser(String user){
+        return System.getProperty("user.name");
     }
 }
